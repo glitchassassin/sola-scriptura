ae36f1b274cdd3907b118a5acea350c39f2e0f76
glitchassassin <jon.winsley@gmail.com>
2017-12-31 15:02:38 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Bible Reader
+
+This is a Python-based (and hence cross-platform) command-line Bible reader.
---
+++ tests/pysword_test.py
@@ -0,0 +1,10 @@
+from pysword.modules import SwordModules
+
+modules = SwordModules("../ESV2011.zip")
+found_modules = modules.parse_modules()
+
+bible = modules.get_bible_from_module("ESV2011")
+#print(dir(bible.get_structure().get_books()))
+#print(bible.get_structure().get_books())
+output = bible.get(books=["john"], chapters=[1])
+print(output)
